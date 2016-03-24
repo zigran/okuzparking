4f4a489bc5c40d34ac196694f75d9267e7523ba4
zigran <babakishiyeva@gmail.com>
2016-03-24 17:37:17 +0400

    Starting project

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+<div class="header">
+    <div class="container">
+	     <div class="logo">
+	     	<img src="images/logo.png">
+	     </div>
+    </div>
+</div>
+<div class="navbar">
+    <div class="container">
+	    <ul class="nav nav-pills">
+  		    <li role="presentation" class="active"><a href="#">ƏSAS SƏHİFƏ</a></li>
+  		    <li role="presentation"><a href="#">BÜTÜN ŞƏKİLLƏR</a></li>
+  		    <li role="presentation"><a href="#">AVTO DAYANACAQLAR</a></li>
+  		    <li role="presentation"><a href="#">TIXAC OLAN KÜÇƏLƏR</a></li>
+        </ul>
+    </div>
+</div>
+
+<div class="photo">
+    <div class="container">
+    <div class="row">
+    	<div class="add col-md-6 col-md-offset-3"></div>
+    	<div class="check"></div>
+    </div>
+    </div>
+</div>
+
+<div class="main">
+	<div class="container">
+		<div class="user"></div>
+		<div class="row">
+			<div class="col-md-4"></div>
+			<div class="col-md-4"></div>
+			<div class="col-md-4"></div>
+		</div>
+		<div class="row">
+			<div></div>
+			<div></div>
+			<div></div>
+		</div>
+		<div class="row">
+			<div></div>
+			<div></div>
+			<div></div>
+		</div>
+		<div class="row">
+			<div></div>
+			<div></div>
+			<div></div>
+		</div>
+		<div class=""></div>
+		<div class="more"></div>
+</div>
+
+<div class="footer">
+    <div class="container"></div>	
+</div>
+
+</body>
+</html>
